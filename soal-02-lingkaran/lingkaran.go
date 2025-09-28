ef85ffe6bab1f701662349fabf3e1689992278c6
RAFI PUTRA EFFENDI <53668664+RafMorningStar@users.noreply.github.com>
2025-09-28 10:57:58 +0000

    feat(soal-02): Implement circle calculation
    - Add Go code to calculate the area and circumference of a circle.
    - Add pseudocode for the circle algorithm.

--- soal-02-lingkaran/lingkaran.go
+++ soal-02-lingkaran/lingkaran.go
@@ -1,7 +1,19 @@
 package main
-import "fmt"
 
-func main () {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var r float64
+
+	fmt.Scan(&r)
+
+	luas := math.Pi * r * r
+	keliling := 2 * math.Pi * r
+
+	fmt.Println(luas)
+	fmt.Println(keliling)
 
 }
--- soal-02-lingkaran/lingkaran_pseudocode.md
+++ soal-02-lingkaran/lingkaran_pseudocode.md
@@ -1,1 +1,13 @@
+PROGRAM lingkaran
+kamus
+   r, luas, keliling: float
 
+algoritma
+input (r)
+
+luas = pi*r*r
+keliling = 2*pi*r
+
+print(keliling)
+print(luas)
+endprogram
