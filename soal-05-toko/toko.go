cca8a1563a6dd862246a8c1e691ba49882093177
jakuchan <114452214+jakuchan@users.noreply.github.com>
2025-09-28 18:53:10 +0700

    Update toko.go

--- soal-05-toko/toko.go
+++ soal-05-toko/toko.go
@@ -2,6 +2,9 @@ package main
 import "fmt"
 
 func main () {
+	var b1, b2, b3 int
 	
-	
+	fmt.Scan(&b1, &b2, &b3)
+			
+	fmt.Println(float64(b1) + (float64(b1) * 5/100), float64(b2) + (float64(b2) * 5/100), float64(b3) + (float64(b3) * 5/100))
 }
